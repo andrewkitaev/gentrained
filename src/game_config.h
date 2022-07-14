364b48395224bc0d6febd31764a21a699a9ec82f
andrewkitaev <creed_kitaev@outlook.com>
2022-07-14 20:06:18 +0300

    Added CMAKE building system

---
+++ src/game_config.h
@@ -0,0 +1,6 @@
+#ifndef GAMECONFIG_FILE
+#define GAMECONFIG_FILE
+
+#define GAME_TITLE "Gen Trained"
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "game_config.h"
+
+int main(int argc, char const *argv[])
+{
+	
+	std::cout << GAME_TITLE << std::endl;
+
+	return 0;
+}
